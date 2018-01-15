dd39bd1b3ab26eac491da46f5ebd030508657e0a
joe44850 <joe44850@gmail.com>
2018-01-14 21:32:17 -0800

    Added some files

---
+++ src/components/Lib/modal.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Modal extends Component {
+
+    render(){
+        return (
+            <div>Modal Window</div>
+        )
+    }
+}
